b4e567b830af3997cff2a01f284302a32484f69b
changxinyu_97@putlook.com <changxinyu_97@outlook.com>
2020-04-19 17:08:17 +0800

    proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,107 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private final int INITIAL_CAPACITY = 8;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0 ? true : false);
+    }
+
+    private void resize() {
+        if (size == items.length) {
+            resizeHelper(items.length * 2);
+        }
+        if (size < items.length / 4 && items.length > 8) {
+            resizeHelper(items.length / 2);
+        }
+    }
+
+    public int minusOne(int index) {
+        return Math.floorMod(index - 1, items.length);
+    }
+
+
+    public int plusOne(int index) {
+        return Math.floorMod(index + 1, items.length);
+    }
+
+    public int plusOne(int index, int length) {
+        return Math.floorMod(index + 1, length);
+    }
+
+    private void resizeHelper(int capacity) {
+        T[] temp = items;
+        int begin = plusOne(nextFirst);
+        int end = minusOne(nextLast);
+        items = (T[]) new Object[capacity];
+        nextFirst = 0;
+        nextLast = 1;
+        for (int i = begin; i != end; i = plusOne(i, temp.length)) {
+            items[nextLast] = temp[i];
+            nextLast = plusOne(nextLast);
+        }
+        items[nextLast] = temp[end];
+        nextLast = plusOne(nextLast);
+    }
+
+
+    public void addFirst(T item) {
+        items[nextFirst] = item;
+        nextFirst = minusOne(nextFirst);
+        size++;
+        resize();
+    }
+
+    public void addLast(T item) {
+        items[nextLast] = item;
+        nextLast = plusOne(nextLast);
+        size++;
+        resize();
+    }
+
+    public void printDeque() {
+        for (int i = plusOne(nextFirst); i != nextLast; i = plusOne(i)) {
+            System.out.print(items[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        T res = items[plusOne(nextFirst)];
+        nextFirst = plusOne(nextFirst);
+        items[nextFirst] = null;
+        size--;
+        resize();
+        return res;
+    }
+
+    public T removeLast() {
+        T res = items[minusOne(nextLast)];
+        nextLast = minusOne(nextLast);
+        items[nextLast] = null;
+        resize();
+        return res;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        index = Math.floorMod(plusOne(nextFirst) + index, items.length);
+        return items[index];
+    }
+}
