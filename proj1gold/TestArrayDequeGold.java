68e40a94143928fc918052202e35f5ba9fdab78f
changxinyu_97@putlook.com <changxinyu_97@outlook.com>
2020-07-06 16:18:12 +0800

    proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -15,11 +15,12 @@ public class TestArrayDequeGold {
             int random = StdRandom.uniform(100);
             ads.addLast(random);
             sad.addLast(random);
+            System.out.println("addLast(" + random + ")");
         }
         for (int i = 0; i < 10; i++) {
             int actual = ads.get(i);
             int expected = sad.get(i);
-            assertEquals("addLast(): "+actual,
+            assertEquals("addLast(): " + actual,
                     expected, actual);
         }
 
@@ -27,11 +28,12 @@ public class TestArrayDequeGold {
             int random = StdRandom.uniform(100);
             ads.addFirst(random);
             sad.addFirst(random);
+            System.out.println("addFirst(" + random + ")");
         }
         for (int i = 0; i < 10; i++) {
             int actual = ads.get(i);
             int expected = sad.get(i);
-            assertEquals("addFirst(): "+actual,
+            assertEquals("addFirst(): " + actual,
                     expected, actual);
         }
 
@@ -44,14 +46,15 @@ public class TestArrayDequeGold {
         for (int i = 0; i < 10; i++) {
             int actual = ads.get(i);
             int expected = sad.get(i);
-            assertEquals("removeFirst(): "+actual,
+            assertEquals("removeFirst(): " + actual,
                     expected, actual);
         }
         for (int i = 0; i < 10; i++) {
             int actual = actualList.get(i);
             int expected = expectedList.get(i);
-            assertEquals("removeFirst(): "+actual,
+            assertEquals("removeFirst(): " + actual,
                     expected, actual);
+            System.out.println("removeFirst()");
         }
 
         actualList.clear();
@@ -61,8 +64,9 @@ public class TestArrayDequeGold {
             expectedList.add(sad.removeLast());
         }
         for (int i = 0; i < 10; i++) {
-            assertEquals("removeLast(): "+actualList.get(i),
+            assertEquals("removeLast(): " + actualList.get(i),
                     expectedList.get(i), actualList.get(i));
+            System.out.println("removeLast()");
         }
     }
 }
